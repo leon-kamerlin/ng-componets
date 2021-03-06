6c1a718e6b43579e20bcf835890cc1c00540d775
leonkamerlinn <leonkamerlin@MacBook-Pro.local>
2021-03-06 11:49:45 +0100

    update

--- projects/ng-components/src/public-api.ts
+++ projects/ng-components/src/public-api.ts
@@ -2,8 +2,5 @@
  * Public API Surface of ng-components
  */
 
-export * from './lib/ng-components.service';
-export * from './lib/ng-components.component';
-export * from './lib/ng-components.module';
 export * from './lib/shell/shell.module';
 export * from './lib/shell/shell.component';
